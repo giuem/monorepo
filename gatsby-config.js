fd3a5a90231554fd7b239100c064df3caab50b2b
Giuem <giuemcom@gmail.com>
2021-05-05 00:50:32 +0800

    fix: temporarily comment some plugins to make build worked

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
+    // "gatsby-plugin-sitemap",
+    // {
+    //   resolve: "gatsby-plugin-manifest",
+    //   options: {
+    //     icon: "src/images/icon.png",
+    //   },
+    // },
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
@@ -35,7 +35,5 @@ module.exports = {
       resolve: `gatsby-plugin-typescript`,
     },
   ],
-  flags: {
-    THE_FLAG: false
-  }
+  flags: {},
 };
