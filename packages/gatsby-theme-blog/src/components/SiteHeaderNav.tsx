cffacd7c32c57d70b86f465372dc8595d2ca719d
Giuem <giuemcom@gmail.com>
2021-06-20 01:18:08 +0800

    feat: nav item add underline class when actived

--- packages/gatsby-theme-blog/src/components/SiteHeaderNav.tsx
+++ packages/gatsby-theme-blog/src/components/SiteHeaderNav.tsx
@@ -9,8 +9,14 @@ export const SiteHeaderNav: React.FC<SiteHeaderNavProps> = ({ items }) => {
     <nav>
       <ul className="flex gap-4 text-xl text-indigo-700">
         {items.map(({ title, href }) => (
-          <li key={href} className="hover:underline">
-            <Link href={href}>{title}</Link>
+          <li key={href}>
+            <Link
+              className="hover:underline"
+              activeClassName="underline"
+              href={href}
+            >
+              {title}
+            </Link>
           </li>
         ))}
       </ul>
