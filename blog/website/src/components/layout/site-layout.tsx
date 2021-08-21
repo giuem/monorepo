40f9e83bf13c181e53a871d6390c943a069deddd
Giuem <giuemcom@gmail.com>
2021-08-22 03:39:18 +0800

    feat: add lang attr

--- blog/website/src/components/layout/site-layout.tsx
+++ blog/website/src/components/layout/site-layout.tsx
@@ -4,6 +4,7 @@ export const SiteLayout: React.FC = ({ children }) => {
   return (
     <div className="max-w-3xl mx-auto px-4 flex flex-col min-h-screen">
       <Helmet>
+        <html lang="zh-CN" />
         <body className="bg-white text-gray-900" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
