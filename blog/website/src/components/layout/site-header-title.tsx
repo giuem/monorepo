62afb0477936b785426df4e2d82aec06f674cdd9
Giuem <giuemcom@gmail.com>
2021-08-23 01:58:24 +0800

    fix: title style

--- blog/website/src/components/layout/site-header-title.tsx
+++ blog/website/src/components/layout/site-header-title.tsx
@@ -9,16 +9,16 @@ export const SiteHeaderTitle: React.FC<SiteHeaderTitleProps> = ({
   title,
   isPostPage,
 }) => {
+  const Title = isPostPage ? 'h3' : 'h1';
+
   return (
-    <Link
-      className="h-12 flex items-center mr-4 from-indigo-600 via-purple-600 to-pink-600 bg-gradient-to-r bg-clip-text text-transparent "
-      href="/"
-    >
-      {isPostPage ? (
-        <h3 className="font-semibold text-4xl">{title}</h3>
-      ) : (
-        <h1 className="font-semibold text-4xl">{title}</h1>
-      )}
+    <Link className="flex items-center mr-4" href="/">
+      <Title
+        className="font-semibold text-4xl from-indigo-600 via-purple-600 to-pink-600 bg-gradient-to-r bg-clip-text text-transparent"
+        style={{ lineHeight: '48px' }}
+      >
+        {title}
+      </Title>
     </Link>
   );
 };
