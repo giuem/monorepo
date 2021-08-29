c804d4fb05d0ee076324aea7dbdc6480ab68b505
giuem <giuemcom@gmail.com>
2021-08-29 18:15:12 +0800

    fix: toggle button add aria-label

--- blog/website/src/components/layout/dark-mode-toggle.tsx
+++ blog/website/src/components/layout/dark-mode-toggle.tsx
@@ -11,6 +11,7 @@ export const DarkModeToggleButton: React.FC = () => {
   return (
     <button
       className="text-lg hover:bg-gray-100 dark:hover:bg-gray-800 active:bg-gray-100 focus:ring-2 bg-gray-50 dark:bg-gray-900 p-2 rounded"
+      aria-label="深浅色主题切换按钮"
       onClick={toggleTheme}
     >
       {theme === 'light' ? <FiSun /> : <FiMoon />}
