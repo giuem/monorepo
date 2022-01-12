0a86ac2800bd6f174f57652d13b163bef60062d1
giuem <giuemcom@gmail.com>
2022-01-13 01:47:36 +0800

    feat: init pure live web

---
+++ apps/pure-live-web/src/components/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="h-10 w-10 text-blue-400">Hello World</div>;
+}
+
+export default App;
---
+++ apps/pure-live-web/src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly PURE_LIVE_API?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
