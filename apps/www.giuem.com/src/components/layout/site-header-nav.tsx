c9b2bfe1115cb5f6a87c90fc9370b7dfa871a0de
giuem <giuemcom@gmail.com>
2022-05-02 12:46:15 +0800

    feat: nav style and dom structure

--- apps/www.giuem.com/src/components/layout/site-header-nav.tsx
+++ apps/www.giuem.com/src/components/layout/site-header-nav.tsx
@@ -6,20 +6,22 @@ type SiteHeaderNavProps = {
 
 export const SiteHeaderNav: React.FC<SiteHeaderNavProps> = ({ items }) => {
   return (
-    <nav className="mt-4 md:-mx-3">
-      <ul className="flex items-center md:space-x-2 space-x-4 text-lg text-indigo-700 dark:text-indigo-200">
-        {items.map(({ title, href }) => (
-          <li key={href}>
-            <Link
-              className="rounded-md md:hover:bg-indigo-50 md:hover:dark:bg-indigo-900 py-2 md:px-3 transition-colors"
-              activeClassName="md:bg-indigo-50 md:dark:bg-indigo-900 md:no-underline underline"
-              href={href}
-            >
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav
+      className="
+      mt-4 md:-mx-3
+      flex items-center md:space-x-2 space-x-4 text-lg text-indigo-700 dark:text-indigo-200
+      "
+    >
+      {items.map(({ title, href }) => (
+        <Link
+          key={href}
+          className="rounded-md md:hover:bg-indigo-50 md:hover:dark:bg-indigo-900 py-1 md:px-3 transition-colors"
+          activeClassName="md:bg-indigo-50 md:dark:bg-indigo-900 md:no-underline underline"
+          href={href}
+        >
+          {title}
+        </Link>
+      ))}
     </nav>
   );
 };
