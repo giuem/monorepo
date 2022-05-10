c97485ba419344b503a7576c396e21238cea10e9
giuem <giuemcom@gmail.com>
2022-05-10 17:41:14 +0800

    fix: disable animation when first render

--- apps/www.giuem.com/src/components/DarkModeToggle.tsx
+++ apps/www.giuem.com/src/components/DarkModeToggle.tsx
@@ -1,9 +1,12 @@
 import { useTheme } from '@giuem/gatsby-plugin-dark-mode';
 import { FiSun, FiMoon } from 'react-icons/fi';
 import { useSpring, animated } from 'react-spring';
+import { useFirstMountState } from 'react-use';
 
 export const DarkModeToggleButton: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
+
+  const isFirstMount = useFirstMountState();
 
   if (theme === undefined) {
     return null;
@@ -18,28 +21,32 @@ export const DarkModeToggleButton: React.FC = () => {
       aria-label="深浅色主题切换按钮"
       onClick={toggleTheme}
     >
-      {theme === 'light' ? <DarkIcon /> : <LightIcon />}
+      {theme === 'light' ? (
+        <DarkIcon animate={!isFirstMount} />
+      ) : (
+        <LightIcon animate={!isFirstMount} />
+      )}
     </button>
   );
 };
 
-function DarkIcon() {
+function DarkIcon({ animate = false }) {
   const props = useSpring({
     from: { rotate: '90deg', scale: 0.8 },
     to: { rotate: '0', scale: 1 },
   });
 
   return (
-    <animated.div style={props}>
+    <animated.div style={animate ? props : undefined}>
       <FiMoon />
     </animated.div>
   );
 }
 
-function LightIcon() {
+function LightIcon({ animate = false }) {
   const props = useSpring({ from: { rotate: '45deg' }, to: { rotate: '0' } });
   return (
-    <animated.div style={props}>
+    <animated.div style={animate ? props : undefined}>
       <FiSun />
     </animated.div>
   );
