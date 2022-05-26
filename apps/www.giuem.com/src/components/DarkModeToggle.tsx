b46543ca3e18dc41030fe1e46f16810b09880802
giuem <giuemcom@gmail.com>
2022-05-27 00:48:43 +0800

    fix: hydrate issue

--- apps/www.giuem.com/src/components/DarkModeToggle.tsx
+++ apps/www.giuem.com/src/components/DarkModeToggle.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from '@giuem/gatsby-plugin-dark-mode';
 import { useSpring, animated } from '@react-spring/web';
+import { useEffect } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
 import { useFirstMountState } from 'react-use';
 
@@ -7,10 +8,6 @@ export const DarkModeToggleButton: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
 
   const isFirstMount = useFirstMountState();
-
-  if (theme === undefined) {
-    return null;
-  }
 
   return (
     <button
@@ -21,32 +18,50 @@ export const DarkModeToggleButton: React.FC = () => {
       aria-label="深浅色主题切换按钮"
       onClick={toggleTheme}
     >
-      {theme === 'light' ? (
-        <DarkIcon animate={!isFirstMount} />
-      ) : (
-        <LightIcon animate={!isFirstMount} />
-      )}
+      {/* {theme === 'light' ? ( */}
+      <DarkIcon animate={!isFirstMount && theme === 'light'} />
+      {/*  ) : ( */}
+      <LightIcon animate={!isFirstMount && theme === 'dark'} />
+      {/* )} */}
     </button>
   );
 };
 
 function DarkIcon({ animate = false }) {
-  const props = useSpring({
-    from: { rotate: '90deg', scale: 0.8 },
-    to: { rotate: '0', scale: 1 },
-  });
+  const [style, api] = useSpring(() => ({
+    rotate: '0',
+    scale: 1,
+  }));
+
+  useEffect(() => {
+    if (animate) {
+      api.start({
+        from: { rotate: '90deg', scale: 0.8 },
+        to: { rotate: '0', scale: 1 },
+      });
+    }
+  }, [animate, api]);
 
   return (
-    <animated.div style={animate ? props : undefined}>
+    <animated.div className="block dark:hidden" style={style}>
       <FiMoon />
     </animated.div>
   );
 }
 
 function LightIcon({ animate = false }) {
-  const props = useSpring({ from: { rotate: '45deg' }, to: { rotate: '0' } });
+  const [style, api] = useSpring(() => ({
+    rotate: '0',
+  }));
+
+  useEffect(() => {
+    if (animate) {
+      api.start({ from: { rotate: '45deg' }, to: { rotate: '0' } });
+    }
+  }, [animate, api]);
+
   return (
-    <animated.div style={animate ? props : undefined}>
+    <animated.div className="hidden dark:block" style={style}>
       <FiSun />
     </animated.div>
   );
